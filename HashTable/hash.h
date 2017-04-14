313b401494419d361ab5bc83d1cd3238273157ea
Nashe <mncubenashe@gmail.com>
2017-04-14 16:15:58 +0100

    Modified header file in HashTable

--- HashTable/hash.h
+++ HashTable/hash.h
@@ -1,6 +1,8 @@
 //
 //  hash.h
-//  
+//  This program implements a basic HashTable for string values.
+//  The programm follows this tutorial https://www.youtube.com/watch?v=MfhjkfocRR0&index=1&list=PLTxllHdfUq4f7-uHOpxXnBUbsuLiI9pmb
+//  Feel free to modify.
 //
 //  Created by nashe mncube on 14/04/2017.
 //
