351d77ed31dde81da346d37cef0726c892f4004a
Nashe <mncubenashe@gmail.com>
2017-04-18 23:32:00 +0100

    Added graph. Dijkstra algo incomplete

--- dijkstra-graph/Dijkstra/src/ds/graph/Vertex.java
+++ dijkstra-graph/Dijkstra/src/ds/graph/Vertex.java
@@ -4,14 +4,17 @@ package ds.graph;
  * Created by nashe on 17/04/2017.
  */
 
+import java.util.Collections;
 import java.util.Objects;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.sun.javafx.collections.ImmutableObservableList;
 import ds.graph.Edge;
 
 public class Vertex {
-    private String label;
-    private List<Edge> edgesFromVertex;
+    private final String label;
+    private final List<Edge> edgesFromVertex;
 
     /**
      * Constructer of class
@@ -31,8 +34,73 @@ public class Vertex {
 
     public List<Edge> getEdgesFromVertex()
     {
-        return this.edgesFromVertex;
+        return Collections.unmodifiableList(this.edgesFromVertex);
     }
 
+    public void addEdge(Edge edge)
+    {
+        Objects.requireNonNull(edge);
+        if(edgesFromVertex.contains(edge))
+            throw new IllegalArgumentException("Trying to add already existing edge");
+        else if(edge.getSource().getLabel() != label && edge.getDestination().getLabel() != label)
+            throw new IllegalArgumentException("Trying to add invalid edge");
+
+        edgesFromVertex.add(edge);
+    }
+
+    public void removeEdge(Edge edge)
+    {
+        Objects.requireNonNull(edge);
+
+    }
+
+    public boolean containsEdge(Edge edge)
+    {
+        Objects.requireNonNull(edge);
+
+        return edgesFromVertex.contains(edge);
+    }
+
+    public Edge getEdge(Edge edge)
+    {
+        Objects.requireNonNull(edge);
+        Edge[] ret = {null};
+        if(!edgesFromVertex.contains(edge))
+            throw new IllegalArgumentException("Trying to obtain non-existing edge");
+        edgesFromVertex.forEach(e ->
+        {
+            if(e.equals(edge))
+                ret[0] = e;
+        });
+
+        return ret[0];
+    }
+
+    public boolean compareWithObject(Object other)
+    {
+        Objects.requireNonNull(other);
+
+        if(!(other instanceof Vertex))
+            return false;
+
+        Vertex v = (Vertex)other;
+
+        return(label.equals(v.label));
+    }
+
+    public int numberOfEdges()
+    {
+        return edgesFromVertex.size();
+    }
+
+    public String toString()
+    {
+        return "Vertex " + this.label;
+    }
+
+    public int HashCode()
+    {
+        return label.hashCode();
+    }
 
 }
