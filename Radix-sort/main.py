71626378a0aa3f93aafccea7d740960350504479
Nashe <mncubenashe@gmail.com>
2017-05-07 15:05:18 +0100

    Added Radix, modified Makefile

---
+++ Radix-sort/main.py
@@ -0,0 +1,60 @@
+"""
+Author: Nashe Mncube
+This script implements the radizx sort algorithm
+Information on the algorithm and it's implementation can be found here
+https://en.wikipedia.org/wiki/Radix_sort
+
+"""
+
+
+def radixSort(aList):
+    RADIX = 10
+    maxLength = False
+    tmp, placement = -1, 1
+
+    while not maxLength:
+        maxLength = True
+        buckets = [list() for _ in range(RADIX)]
+
+        for i in aList:
+            tmp = int((int(i)-97) / placement)
+            buckets[tmp % RADIX].append(i)
+            if maxLength and tmp > 0:
+                maxLength = False
+        a = 0
+        for b in range(RADIX):
+            buck = buckets[b]
+            for i in buck:
+                aList[a] = i
+                a += 1
+        placement *= RADIX
+    return aList
+
+
+sortedList = radixSort([9443,
+7126,
+2938,
+5299,
+6366,
+3917,
+1006,
+3562,
+5498,
+7685,
+84,
+1411,
+8499,
+2595,
+6,
+3708,
+3342,
+1203,
+6896,
+6394,
+3663,
+6818,
+3136,
+1337,
+5000])
+
+print(sortedList)
